c251a35f3e714a52d5fe2223f8243cefebe481f5
eitanLieberman <eitanl95@gmail.com>
2021-12-22 18:38:36 +0200

    shopping categories

--- src/data.js
+++ src/data.js
@@ -6,38 +6,73 @@ export const sliderItems = [
     img: logo,
     title: "DYNAMIC",
     desc: "DONE WITH STYLED COMPONENTS⏰ ",
-    bg: "royalblue",
+    bg: "white",
   },
   {
     id: 2,
     img: logo,
     title: "Sale 2",
     desc: "30% OFF! LIMITED TIME OFFER ⏰ ",
-    bg: "turquoise",
+    bg: "white",
   },
   {
     id: 3,
     img: logo,
     title: "Sale 3",
     desc: "30% OFF! LIMITED TIME OFFER ⏰ ",
-    bg: "mustard",
+    bg: "white",
   },
 ];
 
 export const categories = [
   {
     id: 1,
-    img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    img: "https://i.pinimg.com/564x/99/02/b1/9902b1882b5d39ea8f57b3098fabe9bb.jpg",
     title: "Clothing",
   },
   {
     id: 2,
-    img: "https://images.pexels.com/photos/3345882/pexels-photo-3345882.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    img: "https://i.pinimg.com/564x/e0/de/b1/e0deb12cdc1a986e6c777857f2cf0732.jpg",
     title: "Gadgets",
   },
   {
     id: 3,
-    img: "https://i.pinimg.com/564x/39/1e/d9/391ed9b46de6239fbcc36d12e7d0d541.jpg",
+    img: "https://i.pinimg.com/564x/33/db/58/33db5812232e153415a46f8cfca762c9.jpg",
     title: "Home Gym",
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+  },
+  {
+    id: 2,
+    img: "https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+  },
+  {
+    id: 3,
+    img: "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+  },
+  {
+    id: 4,
+    img: "https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+  },
+  {
+    id: 5,
+    img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+  },
+  {
+    id: 6,
+    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+  },
+  {
+    id: 7,
+    img: "https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+  },
+  {
+    id: 8,
+    img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
   },
 ];
