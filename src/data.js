0804cf3d400cf55bdddb775c848fec8ef0dbc187
eitanLieberman <eitanl95@gmail.com>
2021-12-22 14:54:00 +0200

    react slider

---
+++ src/data.js
@@ -0,0 +1,43 @@
+import logo from "./images/landing3.png";
+
+export const sliderItems = [
+  {
+    id: 1,
+    img: logo,
+    title: "DYNAMIC",
+    desc: "DONE WITH STYLED COMPONENTS⏰ ",
+    bg: "royalblue",
+  },
+  {
+    id: 2,
+    img: logo,
+    title: "Sale 2",
+    desc: "30% OFF! LIMITED TIME OFFER ⏰ ",
+    bg: "turquoise",
+  },
+  {
+    id: 3,
+    img: logo,
+    title: "Sale 3",
+    desc: "30% OFF! LIMITED TIME OFFER ⏰ ",
+    bg: "mustard",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "Clothing",
+  },
+  {
+    id: 2,
+    img: "https://images.pexels.com/photos/3345882/pexels-photo-3345882.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    title: "Gadgets",
+  },
+  {
+    id: 3,
+    img: "https://i.pinimg.com/564x/39/1e/d9/391ed9b46de6239fbcc36d12e7d0d541.jpg",
+    title: "Home Gym",
+  },
+];
