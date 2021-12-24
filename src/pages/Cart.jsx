244450985670d6560935bd42dd7ab2a0ba8ce8d6
eitanLieberman <eitanl95@gmail.com>
2021-12-24 14:39:40 +0200

    shopping cart design

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,232 @@
+import { Add, Remove } from "@material-ui/icons";
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+
+const Container = styled.div``;
+
+const Wrapper = styled.div`
+  padding: 20px;
+`;
+
+const Title = styled.h1`
+  font-weight: 300;
+  text-align: center;
+`;
+const Top = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 20px;
+`;
+const TopButton = styled.button`
+  padding: 10px;
+  font-weight: 600;
+  cursor: pointer;
+  border: ${(props) => props.type === "filled" && "none"};
+  background-color: ${(props) =>
+    props.type === "filled" ? "black" : "transparent"};
+  color: ${(props) => props.type === "filled" && "white"};
+`;
+
+const TopTexts = styled.div``;
+
+const TopText = styled.span`
+  text-decoration: underline;
+  cursor: pointer;
+  margin: 0px 10px;
+`;
+
+const Bottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Info = styled.div`
+  flex: 3;
+`;
+
+const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ProductDetail = styled.div`
+  flex: 2;
+  display: flex;
+`;
+
+const Image = styled.img`
+  width: 200px;
+`;
+
+const Details = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+`;
+
+const ProductName = styled.span``;
+
+const ProductId = styled.span``;
+
+const ProductColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+`;
+
+const ProductSize = styled.span``;
+
+const PriceDetail = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ProductAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const ProductAmount = styled.div`
+  font-size: 24px;
+  margin: 5px;
+`;
+const ProductPrice = styled.div`
+  font-size: 30px;
+  font-weight: 200;
+`;
+
+const Summary = styled.div`
+  flex: 1;
+  border: 0.5px solid lightgray;
+  border-radius: 10px;
+  padding: 20px;
+  height: 50vh;
+`;
+const SummaryTitle = styled.h1`
+  font-weight: 200;
+`;
+const SummaryItem = styled.div`
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${(props) => props.type === "total" && "200"};
+  font-size: ${(props) => props.type === "total" && "24px"};
+`;
+const SummaryItemText = styled.span``;
+const SummaryItemPrice = styled.span``;
+const Button = styled.button`
+  width: 100%;
+  padding: 10px;
+  background-color: black;
+  color: white;
+  font-weight: 600;
+`;
+
+const Cart = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Wrapper>
+        <Title>YOUR CART</Title>
+        <Top>
+          <TopButton>CONTINUE SHOPPING</TopButton>
+          <TopTexts>
+            <TopText>SHOPPING CART(2)</TopText>
+            <TopText>YOUR WISHLIST(0)</TopText>
+          </TopTexts>
+          <TopButton type="filled">CHECKOUT</TopButton>
+        </Top>
+        <Bottom>
+          <Info>
+            <Product>
+              <ProductDetail>
+                <Image src="https://i.pinimg.com/564x/b3/af/81/b3af81144a896127f8399419a328ee70.jpg" />
+                <Details>
+                  <ProductName>
+                    <b>name:</b>FitWatch
+                  </ProductName>
+                  <ProductId>
+                    <b>id:</b>9329488234
+                  </ProductId>
+                  <ProductColor color="black" />
+                  <ProductSize>
+                    <b>size:</b>Men
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>2</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>$200</ProductPrice>
+              </PriceDetail>
+            </Product>
+            <hr />
+            <Product>
+              <ProductDetail>
+                <Image src="https://i.pinimg.com/564x/b3/af/81/b3af81144a896127f8399419a328ee70.jpg" />
+                <Details>
+                  <ProductName>
+                    <b>name:</b>FitWatch
+                  </ProductName>
+                  <ProductId>
+                    <b>id:</b>9329488234
+                  </ProductId>
+                  <ProductColor color="black" />
+                  <ProductSize>
+                    <b>size:</b>Men
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>2</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>$200</ProductPrice>
+              </PriceDetail>
+            </Product>
+          </Info>
+          <Summary>
+            <SummaryTitle>ORDER SUMMARY</SummaryTitle>
+            <SummaryItem>
+              <SummaryItemText>Subtotal</SummaryItemText>
+              <SummaryItemPrice>$ 400</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText> Shipping</SummaryItemText>
+              <SummaryItemPrice>$ 10</SummaryItemPrice>
+            </SummaryItem>
+            <SummaryItem>
+              <SummaryItemText> Discount</SummaryItemText>
+              <SummaryItemPrice>$ -10</SummaryItemPrice>
+            </SummaryItem>
+            <hr />
+            <SummaryItem type="total">
+              <SummaryItemText> Total</SummaryItemText>
+              <SummaryItemPrice>$ 400</SummaryItemPrice>
+            </SummaryItem>
+            <Button>CHECKOUT</Button>
+          </Summary>
+        </Bottom>
+      </Wrapper>
+
+      <Footer />
+    </Container>
+  );
+};
+
+export default Cart;
