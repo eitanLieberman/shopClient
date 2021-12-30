90da518e619020723bc33fa6770d51a52d1863d2
eitanLieberman <eitanl95@gmail.com>
2021-12-30 15:50:07 +0200

    stripe fix final

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -6,7 +6,9 @@ import Navbar from "../components/Navbar";
 import { mobile } from "../responsive";
 import { useSelector } from "react-redux";
 import StripeCheckout from "react-stripe-checkout";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { userRequest } from "../requestMethods";
+import { useHistory } from "react-router";
 
 const KEY = process.env.REACT_APP_STRIPE;
 const Container = styled.div``;
@@ -147,11 +149,32 @@ const Button = styled.button`
 const Cart = () => {
   const cart = useSelector((state) => state.cart);
   const [stripeToken, setStripeToken] = useState(null);
-
+  const history = useHistory();
   const onToken = (token) => {
+    console.log(token);
     setStripeToken(token);
   };
   console.log(stripeToken);
+
+  useEffect(() => {
+    const makeRequest = async () => {
+      try {
+        const res = await userRequest.post("/checkout/payment", {
+          tokenId: stripeToken.id,
+          amount: 500,
+        });
+        history.push("/success", {
+          stripeData: res.data,
+          products: cart,
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    stripeToken && cart.total >= 1 && makeRequest();
+    console.log(cart.total);
+  }, [stripeToken, cart.total, history]);
+
   return (
     <Container>
       <Navbar />
