b76cb14d7b62ca4bcbd97134de0dfd60a5bb4280
eitanLieberman <eitanl95@gmail.com>
2021-12-30 16:59:50 +0200

    redux login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,9 @@
+import { useState } from "react";
 import styled from "styled-components";
 import { mobile } from "../responsive";
+import { useDispatch } from "react-redux";
+import { login } from "../redux/apiCalls";
+import { useSelector } from "react-redux";
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
@@ -45,6 +49,10 @@ const Button = styled.button`
   cursor: pointer;
   margin-bottom: 10px;
   text-align: center;
+  &:disabled {
+    color: green;
+    cursor: not-allowed;
+  }
 `;
 
 const Link = styled.a`
@@ -54,17 +62,43 @@ const Link = styled.a`
   cursor: pointer;
 `;
 
+const Error = styled.span`
+  color: red;
+`;
+
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const { isFetching, error } = useSelector((state) => state.user);
+  const handleClick = (e) => {
+    e.preventDefault();
+    login(dispatch, { username, email, password });
+  };
+
   return (
     <Container>
       <Wrapper>
         <Title>SIGN IN</Title>
 
         <Form>
-          <Input placeholder="email"></Input>
-          <Input placeholder="password"></Input>
+          <Input
+            placeholder="username/email"
+            onChange={(e) => {
+              setUsername(e.target.value);
+              setEmail(e.target.value);
+            }}
+          ></Input>
+          <Input
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+          ></Input>
 
-          <Button>LOGIN</Button>
+          <Button onClick={handleClick} disabled={isFetching}>
+            LOGIN
+          </Button>
+          {error && <Error>Something went wrong</Error>}
           <Link>FORGOT USERNAME?</Link>
           <Link>NEW HERE? SING UP!</Link>
         </Form>
